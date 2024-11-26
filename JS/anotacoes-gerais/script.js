e9111bedf1750e74d9457d40047a40d4d01c667a
ErickGuxta <146475173+ErickGuxta@users.noreply.github.com>
2024-11-25 22:29:22 -0300

    .

--- JS/anotacoes-gerais/script.js
+++ JS/anotacoes-gerais/script.js
@@ -106,4 +106,102 @@ personagem.correr();
 
 
 
+// MÉTODOS DE ARRAY
 
+const numeros = [40, 100, 200, 20, 500, 800];
+
+//Adicionar ou remover itens
+
+    //push
+        numeros.push(55);
+        console.log(numeros);
+    //pop
+        numeros.pop();  //-> remove o último elemento
+        console.log(numeros);
+
+//Função para encontrar maior ou menor valor
+
+    //para maior numero
+    function MaiorNumero(array) {
+        return Math.max.apply(null, array);
+    }
+    //para menor numero
+    function MenorNumero(array) {
+        return Math.min.apply(null, array);
+    }
+
+document.getElementById("maior").innerHTML = "O maior número é: " + MaiorNumero(numeros)
+document.getElementById("menor").innerHTML = "O menor número é: " + MenorNumero(numeros)
+
+
+// fazer uma filtragem 
+    
+    function filtragem(value, index, array) {
+        return value > 20
+    }
+
+    const maior20 = numeros.filter(filtragem);
+
+    document.getElementById("valores-maior20").innerHTML = "Os valores maiores que 20 são: " + maior20
+
+
+
+//If, else
+
+    //exemplo 1
+
+    var usuario = "Eric";
+
+    if(usuario == "Pedro") {
+        console.log("O nome dele é Pedro");
+    } else if(usuario == "Matheus") {
+        console.log("O nome é Matheus");
+    } else if(usuario == "Eric") {
+        console.log("O nome é Eric");
+    } else {
+        console.log("Ele possui outro nome!");
+    }
+
+
+        //exemplo 2 (com formulario)
+
+function verificarTexto() {
+    let texto_input = document.getElementById("texto_input").value;
+
+    if (texto_input == "" || texto_input == null) {
+        // document.getElementById("teste").innerHTML = "Campo obrigatório"
+        let p = document.getElementById("teste");
+        p.innerHTML = "O campo não pode ser vazio"
+        p.style.color = "red"
+    } else{
+
+        let p = document.getElementById("teste");
+        p.innerHTML = "Tudo certo!";
+        p.style.color =  "green"
+    }
+}
+
+//Swith
+
+function verificarCor() {
+    let cor = document.getElementById("cor").value;
+
+    switch (cor) {
+        case "azul":
+            //o que acontece
+            document.body.style.backgroundColor = "blue"
+            break;
+        case "vermelho":
+            //o que acontece
+            document.body.style.backgroundColor = "red"
+            break;
+        default:
+            //o que acontece
+            document.getElementById("teste2").innerHTML = "Cor não disponível"
+            break;
+    }
+
+}
+
+
+//For, while
