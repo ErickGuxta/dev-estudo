9a35e1e9f4b89842db758bae315b7423d4d790c8
ErickGuxta <146475173+ErickGuxta@users.noreply.github.com>
2025-03-23 22:13:08 -0300

    .

--- JS/Booleanos/booleanos.js
+++
@@ -1,5 +0,0 @@
-
-let luzAcesa =  false
-
-let aviso =  !luzAcesa ? "A luz está apagada." : "A luz está acesa"
-console.log(aviso)
--- JS/Pilhas/pilha.js
+++
@@ -1,5 +0,0 @@
-class Pilha {
-    constructor(parameters) {
-        
-    }
-}
