ae1a6996c2c1fdc06aa79dbac32c2e0e1bdea957
ErickGuxta <146475173+ErickGuxta@users.noreply.github.com>
2025-04-02 21:47:21 -0300

    .

--- portifolio-cinthia/scripts/js/script.js
+++ portifolio-cinthia/scripts/js/script.js
@@ -1,23 +1,36 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//     const h1 = document.querySelector(".container-left h1");
-//     const textoOriginal = "Olá, Eu sou Erick";
-//     let i = 0;
+document.addEventListener("DOMContentLoaded", function(){
+    const h1 = document.querySelector(".container-left h1");
+    const textoOriginal = "Olá, Eu sou Cinthia!";
+    let i = 0;
 
-//     h1.textContent = " ";
-//     h1.classList.add("typing");
+    h1.textContent = " ";
+    h1.classList.add("typing");
 
-//     function digitar() {
-//         if (i < textoOriginal.length) {
-//             h1.textContent += textoOriginal.charAt(i);
-//             i++;
-//             setTimeout(digitar, 100); 
-//         } else {
-//             h1.classList.remove("typing"); 
-//         }
-//     }
+    function digitar() {
+        if (i < textoOriginal.length ) {
+            h1.textContent += textoOriginal.charAt(i);
+            i++;
+            setTimeout(digitar, 120);
+        } else{
+            h1.classList.remove("typing");
+        }   
+    }
+    digitar()
+})
 
-//     digitar();
-// });
+document.getElementById("contactForm").addEventListener("submit", async function (element) {
+    element.preventDefault();
 
+    const formData = new FormData(element.target);
+    const response = await fetch("send-email.php", {
+        method: "POST",
+        bady: formData
+    });
 
-
+    const statusMsg = document.getElementById("statusMsg");
+    if (resposta.ok) {
+        statusMsg.textContent = "mensagem enviada com sucesso"
+    } else{
+        statusMsg.textContent = "erro ao enviar mensagem";
+    }    
+})
