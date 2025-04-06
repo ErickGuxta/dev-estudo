d1900fa0179705cac45a7a7b0f56f640fa07be17
ErickGuxta <146475173+ErickGuxta@users.noreply.github.com>
2025-04-06 17:04:08 -0300

    .

---
+++ portifolio-erick/processar_form.php
@@ -0,0 +1,9 @@
+<?php
+
+$from = "gustavocosta.tech@gmail.com";
+
+// capturando json
+$json = file_get_contents('php//input');
+
+
+?>
--- portifolio-erick/scripts/js/script.js
+++ portifolio-erick/scripts/js/script.js
@@ -1,3 +1,4 @@
+// Script para animação de digitação em texto
 document.addEventListener("DOMContentLoaded", function () {
     const h1 = document.querySelector(".container-left h1");
     const textoOriginal = "Olá, Eu sou Erick";
@@ -21,3 +22,58 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
+document.addEventListener('DOMContentLoaded', function (){
+    const formEl = this.getElementById('form')
+    const emailInput = this.getElementById('email')
+
+
+    formEl.addEventListener('submit', evento=>{
+        evento.preventDefault();
+
+        // validação do email
+        if (emailInput.value === "" || !isEmailValid(emailInput.value)) {
+            alert("Digite um email válido")
+            return
+        }
+
+
+        // Transformando dados(objeto) em json 
+        const formData = new FormData(formEl);
+        const data = Object.fromEntries(formData)
+
+        const jsonData = {
+            nome: data['input-text'],
+            email: data['input-email'],
+            mensage: data['input-mensage']
+        };
+        const jsonString = JSON.stringify(jsonData)
+
+        // Enviando dados do json para o php processar USANDO FETCH
+
+        fetch('processar_form.php', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: jsonString 
+        }).then(resposta => resposta.json()).then(data => {
+            alert(data.mensagem)
+        }).catch(error => {
+            alert("Erro ao enviar mensagem")
+            console.log(error)
+        })
+
+    })
+})
+
+// Função para validar email usando regex
+function isEmailValid(email) {
+    const emailRegex = new RegExp(
+        /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/
+    );
+
+    if (emailRegex.test(email)) {
+        return true;
+    }
+    return false;
+}
