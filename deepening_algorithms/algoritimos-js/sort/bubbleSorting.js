400ee092233802c2210bd1bdf1f605b404ff4420
ErickGuxta <146475173+ErickGuxta@users.noreply.github.com>
2025-04-29 00:53:47 -0300

    .

--- deepening_algorithms/algoritimos-js/sort/bubbleSorting.js
+++ deepening_algorithms/algoritimos-js/sort/bubbleSorting.js
@@ -4,7 +4,7 @@ const bubbleSort = arr => {
 
     let n = arr.length
     for (let i = 0; i < n - 1; i++){
-        for (let j = 0; j < n - 1; j++) {
+        for (let j = 0; j < n - 1 - i; j++) {
             if (arr[j] > arr[j + 1] ) {
                 let temporario = arr[j];
 
