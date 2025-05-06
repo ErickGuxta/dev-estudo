548041fa9d33efd3b61d1739ff1179721a844e00
ErickGuxta <146475173+ErickGuxta@users.noreply.github.com>
2025-05-05 21:56:46 -0300

    .

--- deepening_algorithms/algoritimos-js/recursao/fatorial_recusao.js
+++ deepening_algorithms/algoritimos-js/recursao/fatorial_recusao.js
@@ -14,8 +14,8 @@ console.log(fatorial(5));
 // Função fatorial COM recursividade
 
 function fat_recursao(x) {
-    if (x == 1 )return 1
-    else return x * fat_recursao(x -1)
+    if (x == 1 )return 1;
+    else return x * fat_recursao(x -1);
 }
 
 console.log(fat_recursao(5))
