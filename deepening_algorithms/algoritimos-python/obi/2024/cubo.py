bed807dcfdee71d126c62a5b3fc2b120a83d5c4d
ErickGuxta <146475173+ErickGuxta@users.noreply.github.com>
2025-05-22 23:43:55 -0300

    .

---
+++ deepening_algorithms/algoritimos-python/obi/2024/cubo.py
@@ -0,0 +1,14 @@
+from math import *
+
+n = int(input());
+
+if n >= 2 and n <= 100:
+    print(n*n*(n-2))
+    print()
+    print()
+    print(8)
+else:
+    print(0)
+    print(0)
+    print(0)
+    print(8)
