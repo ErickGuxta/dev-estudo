85ae4b2d06f5a0d5517f395386f5cc0e92ae7b2a
ErickGuxta <gustavocostacnn@gmail.com>
2025-12-12 01:59:24 -0300

    herança múltipla

--- python/python25/poo/heranca-multi.py
+++ python/python25/poo/heranca-multi.py
@@ -1,1 +1,58 @@
 print("\n ----HERANÇA MULTIPLA----")
+
+
+class Dispositivo:
+    def __init__(self, nome, marca) -> None:
+        self.nome = nome
+        self.marca = marca
+        self.ligado = False
+
+    def ligar(self):
+        self.ligado = True
+        return f"{self.nome} ligado"
+
+    def deligar(self):
+        self.ligado = False
+        return f"{self.nome} desligado"
+
+
+class Conectavel:
+    def __init__(self) -> None:
+        self.conectado = False
+
+    def conectar(self):
+        self.conectado = True
+        return "Conectado à internet."
+
+    def desconectar(self):
+        self.conectado = False
+        return "Desconectado da internet."
+
+print("\n ----FAZENDO A HERANÇA MULTIPLA----")
+
+class Smartphone(Dispositivo, Conectavel):
+    def __init__(self, nome, marca,modelo, sistema_operacional) -> None:
+        # O método super() é usado para chamar métodos da classe pai, o que é essencial para 
+        #a extensão(acrescentar mais coisa) de comportamentos em subclasses.
+        super().__init__(nome, marca)
+
+        # Atribuutos especificos da classe herdada da classe-pai
+
+        self.modelo = modelo
+        self.sistema_operacional = sistema_operacional
+
+    def fazer_ligacao(self, numero):
+        if self.ligado == False:
+            return "ligue o dispositivo"
+        else:
+            return f"Ligando para {numero}..."
+
+
+
+
+print("\n ----Intanciando a classe com herança múltipla----")
+meu_smartphone = Smartphone("Galaxy S21", "Samsung", "S21", "Android")
+
+print(meu_smartphone.ligar())            # Galaxy S21 ligado.
+print(meu_smartphone.conectar())         # Conectado à internet.
+print(meu_smartphone.fazer_ligacao("123-456-7890"))  # Ligando para 123-456-7890...
--- python/python25/poo/heranca.py
+++ python/python25/poo/heranca.py
@@ -1,8 +1,9 @@
 print("\n ----HERANÇA----")
 
 class Animal:
-    def __init__(self, nome) ->None:
+    def __init__(self, nome, idade) ->None:
         self.nome = nome
+        self.idade = idade
 
     def andar(self):
         print(f"O animal {self.nome} andou")
@@ -13,17 +14,31 @@ class Animal:
 
 '''classe herdada de Animal'''
 class Cachorro(Animal):
+
+    # chamando o construtor da classe pai
+    def __init__(self, nome, idade, raca):
+        # O método super() é usado para chamar métodos da classe pai, o que é essencial para 
+        # a extensão(acrescentar mais coisa) de comportamentos em subclasses.
+        super().__init__(nome, idade)
+        self.raca = raca
+
     def emitir_som(self):
         return "au au au"
 
-
 class Gato(Animal):
+
+    def __init__(self, nome, idade, raca):
+        # chamando o construtor da classe pai
+        super().__init__(nome, idade)
+        self.raca = raca
+
     def emitir_som(self):
         return "miau miau miau"  
 
+
 # Instanciando
-dog = Cachorro(nome="Rex")
-cat = Gato(nome="Felix")
+dog = Cachorro(nome="Rex", idade = 1, raca = "pitbull")
+cat = Gato(nome="Felix", idade = 2, raca = "persa")
 
 print("\n ----Exemplo Polimorfismo----")
 animais = [dog,cat]
