ca3814b6eb04a6511e2b6f29baf5d12eedba7a1f
ErickGuxta <gustavocostacnn@gmail.com>
2026-04-20 23:50:58 -0300

    psw-erick

--- python/psw/djangofutmesa/futmesa/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class FutmesaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'futmesa'
-    verbose_name = 'Futmesa'
--- sistema_estacao/sistema_estacao/wsgi.py
+++
@@ -1,7 +0,0 @@
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sistema_estacao.settings")
-
-application = get_wsgi_application()
